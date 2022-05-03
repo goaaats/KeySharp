223c2c88d6c9f8df1e7c91227e5ee0af39397039
goaaats <goatsdev@protonmail.com>
2022-05-03 15:34:08 +0200

    expose error type and message on exception

--- KeySharp/KeyringException.cs
+++ KeySharp/KeyringException.cs
@@ -7,7 +7,19 @@ namespace KeySharp;
 /// </summary>
 public class KeyringException : Exception
 {
+    /// <summary>
+    /// The type of error that occurred.
+    /// </summary>
+    public ErrorType Type { get; private set; }
+    
+    /// <summary>
+    /// The message returned by the keyring backend.
+    /// </summary>
+    public string BackendMessage { get; private set; }
+    
     internal KeyringException(ErrorType type, string message) : base($"{message} ({type})")
     {
+        Type = type;
+        BackendMessage = message;
     }
 }
